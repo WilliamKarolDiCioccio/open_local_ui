bb636708b8e33787e5a8cc257f24afc6068c4170
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2024-04-18 12:45:44 +0100

    Add process_run package and run 'ollama serve' command in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:process_run/shell.dart';
 import 'package:open_local_ui/layout/dashboard.dart';
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
@@ -9,6 +10,10 @@ void main() async {
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   final savedThemeMode = await AdaptiveTheme.getThemeMode();
+
+  final shell = Shell();
+
+  () async => shell.run('ollama serve');
 
   FlutterNativeSplash.remove();
 
