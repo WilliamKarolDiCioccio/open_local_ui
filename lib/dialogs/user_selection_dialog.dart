aea13278b454fdd94ee6c3680cdafcf8632d2773
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2024-04-20 17:58:58 +0100

    Make user selection dialog a widget of it's own

--- lib/dialogs/user_selection_dialog.dart
+++ lib/dialogs/user_selection_dialog.dart
@@ -2,49 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:open_local_ui/controllers/chat_controller.dart';
 
+class UserSelectionDialog extends StatelessWidget {
+  const UserSelectionDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Select user'),
+      content: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text('Please select a user to continue:'),
+          const SizedBox(height: 16.0),
+          DropdownMenu(
+            inputDecorationTheme: const InputDecorationTheme(
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+              ),
+              floatingLabelBehavior: FloatingLabelBehavior.never,
+            ),
+            enableFilter: true,
+            enableSearch: true,
+            hintText: 'Select a user',
+            initialSelection: context.read<ChatController>().userName,
+            dropdownMenuEntries: const [
+              DropdownMenuEntry(value: 'Default', label: 'Default'),
+            ],
+            onSelected: (value) {
+              return context.read<ChatController>().setUser(value ?? '');
+            },
+          ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('Close'),
+        ),
+      ],
+    );
+  }
+}
+
 Future<void> showUserSelectionDialog(BuildContext context) async {
   return showDialog(
     context: context,
     builder: (context) {
-      return Consumer(
-        builder: (context, value, child) => AlertDialog(
-          title: const Text('Select user'),
-          content: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text('Please select a user to continue:'),
-              const SizedBox(height: 16.0),
-              DropdownMenu(
-                inputDecorationTheme: const InputDecorationTheme(
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                  ),
-                  floatingLabelBehavior: FloatingLabelBehavior.never,
-                ),
-                enableFilter: true,
-                enableSearch: true,
-                hintText: 'Select a user',
-                initialSelection: context.read<ChatController>().userName,
-                dropdownMenuEntries: const [
-                  DropdownMenuEntry(value: 'Default', label: 'Default'),
-                ],
-                onSelected: (value) {
-                  return context.read<ChatController>().setUser(value ?? '');
-                },
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Close'),
-            ),
-          ],
-        ),
-      );
+      return const UserSelectionDialog();
     },
   );
 }
