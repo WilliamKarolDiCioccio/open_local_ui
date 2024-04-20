6026a2652ba50e4e6344cdeda4268c046695c7bf
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2024-04-20 17:31:56 +0100

    Move user selection dialog to separate file

--- lib/layout/dashboard.dart
+++ lib/layout/dashboard.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:unicons/unicons.dart';
 import 'package:open_local_ui/controllers/chat_controller.dart';
 import 'package:open_local_ui/layout/side_menu_base.dart';
 import 'package:open_local_ui/pages/about.dart';
 import 'package:open_local_ui/pages/users.dart';
-import 'package:provider/provider.dart';
-import 'package:unicons/unicons.dart';
 import 'package:open_local_ui/pages/home.dart';
 import 'package:open_local_ui/pages/chat.dart';
 import 'package:open_local_ui/pages/archive.dart';
 import 'package:open_local_ui/pages/models.dart';
 import 'package:open_local_ui/pages/settings.dart';
 import 'package:open_local_ui/components/text_icon_button.dart';
+import 'package:open_local_ui/dialogs/user_selection_dialog.dart';
 
 class DashboardLayout extends StatefulWidget {
   const DashboardLayout({super.key});
@@ -100,7 +101,7 @@ class _DashboardLayoutState extends State<DashboardLayout> {
             ),
             TextIconButtonComponent(
               text: Provider.of<ChatController>(context).userName,
-              onPressed: () => _showUserSelectionDialog(context),
+              onPressed: () => showUserSelectionDialog(context),
               icon: UniconsLine.user,
             ),
             const SizedBox(
@@ -138,41 +139,6 @@ class _DashboardLayoutState extends State<DashboardLayout> {
         SettingsPage(),
         AboutPage(),
       ],
-    );
-  }
-
-  Future<void> _showUserSelectionDialog(BuildContext context) async {
-    return showDialog<void>(
-      context: context,
-      builder: (context) {
-        return Consumer(
-          builder: (context, value, child) => AlertDialog(
-            title: const Text('Select a user'),
-            content: Column(
-              children: [
-                const Text('Please select a user to continue'),
-                const SizedBox(height: 16.0),
-                DropdownMenu(
-                  inputDecorationTheme: const InputDecorationTheme(
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                      ),
-                      floatingLabelBehavior: FloatingLabelBehavior.never),
-                  enableFilter: true,
-                  enableSearch: true,
-                  hintText: 'Select a user',
-                  initialSelection: context.read<ChatController>().userName,
-                  dropdownMenuEntries: const [
-                    DropdownMenuEntry(value: 'Default', label: 'Default'),
-                  ],
-                  onSelected: (value) =>
-                      context.read<ChatController>().setUser(value ?? ''),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
     );
   }
 }
