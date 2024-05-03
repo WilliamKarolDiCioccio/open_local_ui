7c77db92325025b09edd9e004bb57f526e703a56
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2024-05-04 00:10:52 +0100

    Forgot Windows doesn't like symbols in paths

--- lib/utils/logger.dart
+++ lib/utils/logger.dart
@@ -1,8 +1,9 @@
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:logger/logger.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'package:open_local_ui/helpers/datetime.dart';
 
 final logger = Logger(
   filter: null,
@@ -11,23 +12,24 @@ final logger = Logger(
     printEmojis: false,
     printTime: true,
   ),
-  output: kDebugMode ? null : FileOutput(file: _createLogFile()),
+  output: FileOutput(file: _createLogFile()),
 );
 
 File _createLogFile() {
-  final timeStamp =
-      DateTime.now().toString().split('.').first.replaceAll(' ', '_');
-  final fileName = 'log_$timeStamp.txt';
+  final timeStamp = DateTimeHelpers.getFormattedDateTime()
+      .replaceAll(' ', '_')
+      .replaceAll('/', '-')
+      .replaceAll(':', '-');
+
+  final fileName = 'log_$timeStamp.log';
   var logsFolderPath = '';
 
-  if (!kIsWeb) {
-    getApplicationDocumentsDirectory().then((directory) {
-      logsFolderPath = '${directory.path}/open_local_ui/logs';
-      Directory(logsFolderPath).create(recursive: true).then((_) {
-        return File('$logsFolderPath/$fileName');
-      });
+  getApplicationSupportDirectory().then((directory) {
+    logsFolderPath = '${directory.path}/logs';
+    Directory(logsFolderPath).create(recursive: true).then((_) {
+      return File('$logsFolderPath/$fileName');
     });
-  }
+  });
 
   return File('$logsFolderPath/$fileName');
 }
