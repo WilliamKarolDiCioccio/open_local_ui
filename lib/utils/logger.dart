8ee58f66596d2654547968249be0f5655538de08
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2024-05-03 22:32:32 +0100

    Update logger configuration for log file writing

--- lib/utils/logger.dart
+++ lib/utils/logger.dart
@@ -1,7 +1,33 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:logger/logger.dart';
 
 final logger = Logger(
   filter: null,
-  printer: PrettyPrinter(),
-  output: null,
+  printer: PrettyPrinter(
+    lineLength: 80,
+    printEmojis: false,
+    printTime: true,
+  ),
+  output: kDebugMode ? null : FileOutput(file: _createLogFile()),
 );
+
+File _createLogFile() {
+  final timeStamp =
+      DateTime.now().toString().split('.').first.replaceAll(' ', '_');
+  final fileName = 'log_$timeStamp.txt';
+  var logsFolderPath = '';
+
+  if (!kIsWeb) {
+    getApplicationDocumentsDirectory().then((directory) {
+      logsFolderPath = '${directory.path}/open_local_ui/logs';
+      Directory(logsFolderPath).create(recursive: true).then((_) {
+        return File('$logsFolderPath/$fileName');
+      });
+    });
+  }
+
+  return File('$logsFolderPath/$fileName');
+}
