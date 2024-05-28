218d468d346db52d0bd41fd5efa09b1925b5f163
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2024-05-28 09:41:06 +0100

    Update server build script and fix gRPC code generation

--- scripts/generate_grpc_app_files.ps1
+++ scripts/generate_grpc_app_files.ps1
@@ -3,7 +3,7 @@ $protobufFilesPath = ".\protobufs"
 $serverProtoFileName = "server.proto"
 
 # Generate the gRPC code using the protoc compiler
-protoc --dart_out=./app/lib/services "$protobufFilesPath\$serverProtoFileName"
+protoc --dart_out=grpc:./app/lib/services "$protobufFilesPath\$serverProtoFileName"
 
 # Output a message indicating that the code generation process is complete
 Write-Output "Code generation complete. The executable has been moved to $targetDir"
