716edc4875ddd96af6aedc1e047975876d85c7f1
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2024-08-13 16:40:01 +0200

    Typewrite effect component

---
+++ app/lib/components/typewriter_text.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class TypewriterText extends StatefulWidget {
+  final String text;
+  final Duration duration;
+
+  const TypewriterText({
+    Key? key,
+    required this.text,
+    this.duration = const Duration(milliseconds: 2000),
+  }) : super(key: key);
+
+  @override
+  _TypewriterTextState createState() => _TypewriterTextState();
+}
+
+class _TypewriterTextState extends State<TypewriterText>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<int> _characterCount;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: widget.duration,
+      vsync: this,
+    );
+
+    _characterCount = StepTween(
+      begin: 0,
+      end: widget.text.length,
+    ).animate(_controller);
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _characterCount,
+      builder: (context, child) {
+        String visibleText = widget.text.substring(0, _characterCount.value);
+        return Text(
+          visibleText,
+          textAlign: TextAlign.center,
+        );
+      },
+    );
+  }
+}
