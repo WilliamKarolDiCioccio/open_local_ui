70aafa753920103f50be82532109b56af4ae84cd
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2024-08-14 18:37:51 +0200

    Move WindowManagementBar to its own file

---
+++ app/lib/frontend/widgets/window_management_bar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:system_theme/system_theme.dart';
+
+class WindowManagementBar extends StatelessWidget {
+  const WindowManagementBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return WindowTitleBarBox(
+      child: Row(
+        children: [
+          Flexible(
+            child: MoveWindow(),
+          ),
+          Row(
+            children: [
+              MinimizeWindowButton(
+                colors: WindowButtonColors(
+                  iconNormal: SystemTheme.accentColor.accent,
+                  iconMouseOver: Colors.green,
+                ),
+              ),
+              MaximizeWindowButton(
+                colors: WindowButtonColors(
+                  iconNormal: SystemTheme.accentColor.accent,
+                  iconMouseOver: Colors.orange,
+                ),
+              ),
+              CloseWindowButton(
+                colors: WindowButtonColors(
+                  iconNormal: SystemTheme.accentColor.accent,
+                  iconMouseOver: Colors.red,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
